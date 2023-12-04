9b68d24511d98f91b71be872285c561c9e2ff702
bleuoutremer <bleu.outremer@icloud.com>
2023-12-04 17:18:59 +0100

    update

--- pre_eff.py
+++ pre_eff.py
@@ -1,43 +1,48 @@
-import pandas as pd
 import os
+import pandas as pd
 
 
 def process_sequence(file_path, threshold):
-    rows = []
     with open(file_path, 'r') as file:
-        for line in file:
-            row = line.strip().split('\t')
-            rows.append(row)
-
-    start_index = [i for i, row in enumerate(rows) if '@data' in row][0]
-    data = rows[start_index + 1:]
-    data_df = pd.DataFrame(data)
-    activity_counts = data_df[4].value_counts()
+        line = file.readlines()
+    start_index = line.index('@data\n') + 1
+    data_lines = line[start_index:]
+    df = pd.DataFrame([line.strip().split('\t') for line in data_lines if line.strip() != ''],
+                      columns=['Hand', 'ObjectActedOn', 'ObjectInHand', 'HandState', 'Activity'])
+    activity_counts = df['Activity'].value_counts()
     filtered_activities = activity_counts[activity_counts >= threshold].index.tolist()
-    filtered = data_df[data_df[4].isin(filtered_activities)]
-    filtered_df = filtered.drop_duplicates()
-
-    variable = ["Hand", "ObjectActedOn", "ObjectInHand", "HandState", "Activity"]
-    filtered_df.columns = variable
+    sequence = df[df['Activity'].isin(filtered_activities)]
+    sequence = sequence.loc[(sequence.shift() != sequence).any(axis=1)]
+    sequence = filter_sequence(sequence)
     hand = "Right hand" if "R" in file_path else "Left hand"
-    print(f"{hand} sequence:\n")
-    for i in range(len(filtered_df)):
-        activity = filtered_df.iloc[i]['Activity']
-        precondition_rows = filtered_df.iloc[:i] if i > 0 else filtered_df.iloc[:1]
-        precondition = {col: row[col] for row in precondition_rows.to_dict('records')
-                        for col in filtered_df.columns[:-1]}
-        effect = {col: filtered_df.iloc[i][col] for col in filtered_df.columns[:-1]}
-
-        print(f"Activity {i+1}:", activity)
-        print("Precondition:", precondition)
-        print("Effect:", effect)
-        print()
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 1000)
+    print(f"{hand} sequence:\n", sequence)
+    return sequence
 
 
-path = "data/task_graph/2023-11-27-15-09-22"
-file_names = ["data4testing_R.txt", "data4testing_L.txt"]
-threshold = 150
+def filter_sequence(df, ignore_activity='IdleMotion'):
+    filtered_rows = []
+    idle_motion_seen = False
+    is_sequence_start = True
+    for index, row in df.iterrows():
+        current_activity = row['Activity']
+        if current_activity == ignore_activity:
+            if is_sequence_start or not idle_motion_seen:
+                idle_motion_seen = True
+                filtered_rows.append(row)
+            continue
+        else:
 
-for file_name in file_names:
+            is_sequence_start = False
+
+        filtered_rows.append(row)
+    return pd.DataFrame(filtered_rows)
+
+
+path = 'data/task_graph/2023-11-27-15-09-22'
+threshold = 30
+for file_name in ['data4testing_R.txt', 'data4testing_L.txt']:
     file_path = os.path.join(path, file_name)
-    process_sequence(file_path, threshold)
+    sequence = process_sequence(file_path, threshold)
