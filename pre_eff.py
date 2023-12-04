da392b1b9b28d92df9030cbe67b31a075a5eeb23
bleuoutremer <bleu.outremer@icloud.com>
2023-12-04 14:24:52 +0100

    update

--- pre_eff.py
+++ pre_eff.py
@@ -16,16 +16,28 @@ def process_sequence(file_path, threshold):
     filtered_activities = activity_counts[activity_counts >= threshold].index.tolist()
     filtered = data_df[data_df[4].isin(filtered_activities)]
     filtered_df = filtered.drop_duplicates()
-    hand = "Right hand" if "R" in file_path else "Left hand"
+
     variable = ["Hand", "ObjectActedOn", "ObjectInHand", "HandState", "Activity"]
     filtered_df.columns = variable
-    # print(f"{hand} sequence for {os.path.basename(file_path)}:\n", filtered_df[4])
-    print(f"{hand} sequence:\n", filtered_df)
+    hand = "Right hand" if "R" in file_path else "Left hand"
+    print(f"{hand} sequence:\n")
+    for i in range(len(filtered_df)):
+        activity = filtered_df.iloc[i]['Activity']
+        precondition_rows = filtered_df.iloc[:i] if i > 0 else filtered_df.iloc[:1]
+        precondition = {col: row[col] for row in precondition_rows.to_dict('records')
+                        for col in filtered_df.columns[:-1]}
+        effect = {col: filtered_df.iloc[i][col] for col in filtered_df.columns[:-1]}
+
+        print(f"Activity {i+1}:", activity)
+        print("Precondition:", precondition)
+        print("Effect:", effect)
+        print()
 
 
 path = "data/task_graph/2023-11-27-15-09-22"
 file_names = ["data4testing_R.txt", "data4testing_L.txt"]
 threshold = 150
+
 for file_name in file_names:
     file_path = os.path.join(path, file_name)
     process_sequence(file_path, threshold)
