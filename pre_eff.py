923b01bb32171c13d6a2907c33b0d03d36b60297
bleuoutremer <bleu.outremer@icloud.com>
2023-12-04 20:24:39 +0100

    update

--- pre_eff.py
+++ pre_eff.py
@@ -2,7 +2,11 @@ import os
 import pandas as pd
 
 
-def process_sequence(file_path, threshold):
+def update_dict(dict1, dict2):
+    return {k: v for k, v in dict1.items() if dict2.get(k) == v}
+
+
+def read_data(file_path, threshold):
     with open(file_path, 'r') as file:
         line = file.readlines()
     start_index = line.index('@data\n') + 1
@@ -11,38 +15,72 @@ def process_sequence(file_path, threshold):
                       columns=['Hand', 'ObjectActedOn', 'ObjectInHand', 'HandState', 'Activity'])
     activity_counts = df['Activity'].value_counts()
     filtered_activities = activity_counts[activity_counts >= threshold].index.tolist()
-    sequence = df[df['Activity'].isin(filtered_activities)]
-    sequence = sequence.loc[(sequence.shift() != sequence).any(axis=1)]
-    sequence = filter_sequence(sequence)
-    hand = "Right hand" if "R" in file_path else "Left hand"
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.width', 1000)
-    print(f"{hand} sequence:\n", sequence)
+    filtered_df = df[df['Activity'].isin(filtered_activities)]
+    return filtered_df
+
+
+def only_idlemotion(file_path, threshold):
+    filtered_df = read_data(file_path, threshold)
+    return set(filtered_df['Activity']) == {'IdleMotion'}
+
+
+def process_sequence(file_path, threshold):
+    filtered_df = read_data(file_path, threshold)
+    sequence = filtered_df.loc[(filtered_df.shift() != filtered_df).any(axis=1)]
+    sequence = extract(sequence)
+    # hand = "Right hand" if "R" in file_path else "Left hand"
+    # print(f"{hand} sequence:\n", sequence)
     return sequence
 
 
-def filter_sequence(df, ignore_activity='IdleMotion'):
-    filtered_rows = []
-    idle_motion_seen = False
-    is_sequence_start = True
-    for index, row in df.iterrows():
-        current_activity = row['Activity']
-        if current_activity == ignore_activity:
-            if is_sequence_start or not idle_motion_seen:
-                idle_motion_seen = True
-                filtered_rows.append(row)
-            continue
+def extract(df):
+    activities = df['Activity']
+    buffer = {}
+    for activity in activities:
+        activity_df = df[df['Activity'] == activity]
+        precondition = activity_df.iloc[0][0:4].to_dict()
+        effect = activity_df.iloc[-1][0:4].to_dict()
+        if activity not in buffer:
+            buffer[activity] = {
+                'precondition': precondition,
+                'effect': effect
+            }
         else:
+            previous_precondition = buffer[activity]['precondition']
+            previous_effect = buffer[activity]['effect']
+            updated_precondition = update_dict(previous_precondition, precondition)
+            updated_effect = update_dict(previous_effect, effect)
+            buffer[activity] = {
+                'precondition': updated_precondition,
+                'effect': updated_effect
+            }
+    return buffer
 
-            is_sequence_start = False
 
-        filtered_rows.append(row)
-    return pd.DataFrame(filtered_rows)
+def merge(path, threshold):
+    all_activities = {}
+    for file_name in ['data4testing_R.txt', 'data4testing_L.txt']:
+        file_path = os.path.join(path, file_name)
+        if not only_idlemotion(file_path, threshold):
+            activities = process_sequence(file_path, threshold)
+            for activity, details in activities.items():
+                if activity not in all_activities:
+                    all_activities[activity] = details
+                else:
+                    existing_precondition = all_activities[activity]['precondition']
+                    existing_effect = all_activities[activity]['effect']
+                    updated_precondition = update_dict(existing_precondition, details['precondition'])
+                    updated_effect = update_dict(existing_effect, details['effect'])
+                    all_activities[activity] = {
+                        'precondition': updated_precondition,
+                        'effect': updated_effect
+                    }
+    return all_activities
 
 
 path = 'data/task_graph/2023-11-27-15-09-22'
 threshold = 30
-for file_name in ['data4testing_R.txt', 'data4testing_L.txt']:
-    file_path = os.path.join(path, file_name)
-    sequence = process_sequence(file_path, threshold)
+rich_info = merge(path, threshold)
+print(rich_info)
+
+
