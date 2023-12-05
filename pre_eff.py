047b78785ecfae167ab5c7ce97481387f78ef420
bleuoutremer <bleu.outremer@icloud.com>
2023-12-05 14:22:54 +0100

    update

--- pre_eff.py
+++ pre_eff.py
@@ -2,85 +2,126 @@ import os
 import pandas as pd
 
 
+def flatten_and_unique(lst):
+    result = []
+    for item in lst:
+        if isinstance(item, list):
+            result.extend(flatten_and_unique(item))
+        else:
+            result.append(item)
+    return list(set(result))
+
+
 def update_dict(dict1, dict2):
-    return {k: v for k, v in dict1.items() if dict2.get(k) == v}
+    result = {}
+    all_keys = set(dict1).union(set(dict2))
+    for key in all_keys:
+        values = []
+        if key in dict1:
+            values.append(dict1[key])
+        if key in dict2:
+            values.append(dict2[key])
+        result[key] = values
+    return result
 
 
-def read_data(file_path, threshold):
-    with open(file_path, 'r') as file:
-        line = file.readlines()
-    start_index = line.index('@data\n') + 1
-    data_lines = line[start_index:]
-    df = pd.DataFrame([line.strip().split('\t') for line in data_lines if line.strip() != ''],
-                      columns=['Hand', 'ObjectActedOn', 'ObjectInHand', 'HandState', 'Activity'])
-    activity_counts = df['Activity'].value_counts()
-    filtered_activities = activity_counts[activity_counts >= threshold].index.tolist()
-    filtered_df = df[df['Activity'].isin(filtered_activities)]
-    return filtered_df
-
-
-def only_idlemotion(file_path, threshold):
-    filtered_df = read_data(file_path, threshold)
-    return set(filtered_df['Activity']) == {'IdleMotion'}
-
-
-def process_sequence(file_path, threshold):
-    filtered_df = read_data(file_path, threshold)
-    sequence = filtered_df.loc[(filtered_df.shift() != filtered_df).any(axis=1)]
-    sequence = extract(sequence)
-    # hand = "Right hand" if "R" in file_path else "Left hand"
-    # print(f"{hand} sequence:\n", sequence)
-    return sequence
+def simplify_dict(dict1, dict2):
+    combined_dict = update_dict(dict1, dict2)
+    return {k: flatten_and_unique(v) for k, v in combined_dict.items()}
 
 
 def extract(df):
     activities = df['Activity']
+    last_non_idle_index = (activities != 'IdleMotion').to_numpy().nonzero()[0][-1]
+    if last_non_idle_index < len(activities) - 1:
+        df = df.iloc[:last_non_idle_index + 1]
+    df = df.reset_index(drop=True)
     buffer = {}
-    for activity in activities:
-        activity_df = df[df['Activity'] == activity]
-        precondition = activity_df.iloc[0][0:4].to_dict()
-        effect = activity_df.iloc[-1][0:4].to_dict()
-        if activity not in buffer:
-            buffer[activity] = {
-                'precondition': precondition,
-                'effect': effect
-            }
-        else:
-            previous_precondition = buffer[activity]['precondition']
-            previous_effect = buffer[activity]['effect']
-            updated_precondition = update_dict(previous_precondition, precondition)
-            updated_effect = update_dict(previous_effect, effect)
-            buffer[activity] = {
-                'precondition': updated_precondition,
-                'effect': updated_effect
-            }
-    return buffer
-
-
-def merge(path, threshold):
-    all_activities = {}
-    for file_name in ['data4testing_R.txt', 'data4testing_L.txt']:
-        file_path = os.path.join(path, file_name)
-        if not only_idlemotion(file_path, threshold):
-            activities = process_sequence(file_path, threshold)
-            for activity, details in activities.items():
-                if activity not in all_activities:
-                    all_activities[activity] = details
+    last_activity = None
+    start_index = 0
+    activities = activities.reset_index(drop=True)
+    for index, activity in activities.items():
+        if activity != last_activity:
+            if last_activity is not None:
+                activity_df = df.iloc[start_index:index]
+                precondition = activity_df.iloc[0][0:4].to_dict()
+                effect = activity_df.iloc[-1][0:4].to_dict()
+                if last_activity not in buffer:
+                    buffer[last_activity] = {
+                        'precondition': precondition,
+                        'effect': effect
+                    }
                 else:
-                    existing_precondition = all_activities[activity]['precondition']
-                    existing_effect = all_activities[activity]['effect']
-                    updated_precondition = update_dict(existing_precondition, details['precondition'])
-                    updated_effect = update_dict(existing_effect, details['effect'])
-                    all_activities[activity] = {
+                    previous_precondition = buffer[last_activity]['precondition']
+                    previous_effect = buffer[last_activity]['effect']
+                    updated_precondition = simplify_dict(previous_precondition, precondition)
+                    updated_effect = simplify_dict(previous_effect, effect)
+                    buffer[last_activity] = {
                         'precondition': updated_precondition,
                         'effect': updated_effect
                     }
-    return all_activities
+            last_activity = activity
+            start_index = index
+    return buffer
 
 
-path = 'data/task_graph/2023-11-27-15-09-22'
-threshold = 30
-rich_info = merge(path, threshold)
-print(rich_info)
+
+class ActivityProcessor:
+    def __init__(self, path, threshold):
+        self.path = path
+        self.threshold = threshold
+        self.rich_info = self.merge()
+
+    def read_data(self, file_path):
+        with open(file_path, 'r') as file:
+            line = file.readlines()
+        start_index = line.index('@data\n') + 1
+        data_lines = line[start_index:]
+        df = pd.DataFrame([line.strip().split('\t') for line in data_lines if line.strip() != ''],
+                          columns=['Hand', 'ObjectActedOn', 'ObjectInHand', 'HandState', 'Activity'])
+        activity_counts = df['Activity'].value_counts()
+        filtered_activities = activity_counts[activity_counts >= self.threshold].index.tolist()
+        filtered_df = df[df['Activity'].isin(filtered_activities)].copy()
+        filtered_df.loc[filtered_df['ObjectInHand'] == 'Main Camera', 'ObjectInHand'] = 'NONE'
+        filtered_df.loc[filtered_df['ObjectActedOn'] != 'NONE', 'ObjectActedOn'] = 'Something'
+        filtered_df.loc[filtered_df['Hand'] == 'ToolUse', 'Hand'] = 'Moving'
+        filtered_df.loc[filtered_df['Hand'] == 'Move', 'Hand'] = 'Moving'
+        return filtered_df
+
+    def only_idlemotion(self, file_path):
+        filtered_df = self.read_data(file_path)
+        return set(filtered_df['Activity']) == {'IdleMotion'}
+
+    def process_sequence(self, file_path):
+        filtered_df = self.read_data(file_path)
+        sequence = filtered_df.loc[(filtered_df.shift() != filtered_df).any(axis=1)]
+        sequence = extract(sequence)
+        return sequence
+
+    def merge(self):
+        buffer = {}
+        for file_name in ['data4testing_R.txt', 'data4testing_L.txt']:
+            file_path = os.path.join(self.path, file_name)
+            if not self.only_idlemotion(file_path):
+                dicts = self.process_sequence(file_path)
+                for activity, info in dicts.items():
+                    if activity not in buffer:
+                        buffer[activity] = info
+                    else:
+                        existing_precondition = buffer[activity]['precondition']
+                        existing_effect = buffer[activity]['effect']
+                        updated_precondition = simplify_dict(existing_precondition, info['precondition'])
+                        updated_effect = simplify_dict(existing_effect, info['effect'])
+                        buffer[activity] = {
+                            'precondition': updated_precondition,
+                            'effect': updated_effect
+                        }
+        return buffer
 
 
+if __name__ == "__main__":
+    path = 'data/task_graph/multiple_demos'
+    threshold = 30
+    processor = ActivityProcessor(path, threshold)
+    rich_info = processor.rich_info
+    print(rich_info)
---
+++ visual_search_v2.py
@@ -0,0 +1,274 @@
+import tkinter as tk
+from tkinter import messagebox
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import networkx as nx
+import time
+import numpy as np
+import my_networkx as my_nx
+import random
+from pre_eff import ActivityProcessor
+
+
+def refine(effect, precondition):
+    for key, value in precondition.items():
+        if key not in effect or effect[key] != value:
+            return False
+    return True
+
+
+def match(precondition, effect):
+    for key, precondition_values in precondition.items():
+        if key in effect:
+            effect_values = effect[key]
+            if not any(value in effect_values for value in precondition_values):
+                return False
+        else:
+            return False
+    return True
+
+
+def parse_graph(file_path, rich_info):
+    graph = nx.DiGraph()
+    with open(file_path, 'r') as file:
+        current_node = None
+        neighbors = []
+        weights = []
+
+        for line in file:
+            if 'name:' in line:
+                if current_node and neighbors:
+                    for neighbor, weight in zip(neighbors, weights):
+                        eff = rich_info[current_node]['effect']
+                        pre = rich_info[current_node]['precondition']
+                        if match(pre, eff):
+                            graph.add_edge(current_node, neighbor, weight=weight)
+                        else:
+                            print("Remove edge: \n")
+                            print("Current node: ", current_node)
+                            print("effect: ", eff)
+                            print("neighbor: ", neighbor)
+                            print("precondition: ", pre)
+                current_node = line.split(':')[1].strip()
+                neighbors = []
+                weights = []
+            elif 'neighbors[' in line and current_node:
+                if ':' in line:
+                    neighbor = line.split(':')[1].strip()
+                    neighbors.append(neighbor)
+            elif 'weights[' in line and current_node:
+                if ':' in line:
+                    weight = int(line.split(':')[1].strip())
+                    weights.append(weight)
+
+        if current_node and neighbors:
+            for neighbor, weight in zip(neighbors, weights):
+                graph.add_edge(current_node, neighbor, weight=weight)
+    return graph
+
+
+
+def build_graph(graph, delay):
+    dynamic_graph = nx.DiGraph()
+    for node in graph.nodes:
+        if node not in dynamic_graph:
+            dynamic_graph.add_node(node)
+            draw_graph(dynamic_graph, highlight=[node], update=True)
+            time.sleep(delay)
+
+        for neighbor in graph.neighbors(node):
+            if neighbor not in dynamic_graph:
+                dynamic_graph.add_edge(
+                    node, neighbor, weight=graph[node][neighbor]['weight'])
+                dynamic_graph.add_node(neighbor)
+                draw_graph(dynamic_graph, highlight=[
+                           (node, neighbor), neighbor], update=True)
+                time.sleep(delay)
+            elif (node, neighbor) not in dynamic_graph.edges:
+                dynamic_graph.add_edge(
+                    node, neighbor, weight=graph[node][neighbor]['weight'])
+                draw_graph(dynamic_graph, highlight=[
+                           (node, neighbor), neighbor], update=True)
+                time.sleep(delay)
+    draw_graph(dynamic_graph, update=True)
+    messagebox.showinfo("Info", "Graph construction complete.")
+
+
+def draw_graph(graph, highlight=None, update=False):
+    ax.clear()
+    node_size = 600
+    nx.draw_networkx_nodes(graph, pos, node_size=node_size, node_color=node_color, ax=ax)
+    nx.draw_networkx_labels(graph, pos, ax=ax)
+
+    edge_weights = nx.get_edge_attributes(graph, 'weight')
+    curved_edges = [edge for edge in graph.edges() if reversed(edge) in graph.edges() and edge in edge_weights]
+    straight_edges = list(set(graph.edges()) - set(curved_edges))
+
+    nx.draw_networkx_edges(graph, pos, ax=ax, edgelist=straight_edges, edge_color=edge_color, width=2, arrowstyle='-|>', arrowsize=15)
+    arc_rad = 0.1
+    nx.draw_networkx_edges(graph, pos, ax=ax, edgelist=curved_edges, connectionstyle=f'arc3, rad = {arc_rad}', edge_color=edge_color, width=2, arrowstyle='-|>', arrowsize=15)
+
+    if highlight:
+        for item in highlight:
+            if isinstance(item, tuple):
+                if item in curved_edges or (item[1], item[0]) in curved_edges:
+                    nx.draw_networkx_edges(graph, pos, edgelist=[item], edge_color=highlight_color, width=3,
+                                           arrowstyle='-|>', arrowsize=20, ax=ax, connectionstyle=f'arc3, rad = {arc_rad}')
+                else:
+                    nx.draw_networkx_edges(graph, pos, edgelist=[item], edge_color=highlight_color, width=3,
+                                           arrowstyle='-|>', arrowsize=20, ax=ax)
+            else:
+                nx.draw_networkx_nodes(graph, pos, nodelist=[item], node_size=node_size, node_color=highlight_color, ax=ax)
+
+    curved_edge_labels = {edge: edge_weights.get(edge) for edge in curved_edges}
+    straight_edge_labels = {edge: edge_weights.get(edge) for edge in straight_edges}
+    my_nx.my_draw_networkx_edge_labels(graph, pos, ax=ax, edge_labels=curved_edge_labels, font_size=12, rotate=False, rad=arc_rad)
+    nx.draw_networkx_edge_labels(graph, pos, ax=ax, edge_labels=straight_edge_labels, font_size=12, rotate=False)
+
+    if start_node is not None:
+        nx.draw_networkx_nodes(graph, pos, nodelist=[start_node], node_size=node_size, node_color=start_color, ax=ax)
+    if end_node is not None:
+        nx.draw_networkx_nodes(graph, pos, nodelist=[end_node], node_size=node_size, node_color=end_color, ax=ax)
+
+    canvas.draw()
+    if update:
+        root.update()
+
+
+def draw_path(G, path, ax, pos):
+    edges = [(path[i], path[i + 1]) for i in range(len(path) - 1)]
+    arc_rad = 0.1
+
+    for edge in edges:
+        if (edge[1], edge[0]) in G.edges():
+            nx.draw_networkx_edges(G, pos, edgelist=[edge], edge_color=highlight_color, width=3,
+                                   ax=ax, connectionstyle=f'arc3, rad = {arc_rad}')
+        else:
+            nx.draw_networkx_edges(G, pos, edgelist=[edge], edge_color=highlight_color, width=3, ax=ax)
+
+
+def nx_to_matrix(graph):
+    nodes = list(graph.nodes())
+    num_nodes = len(nodes)
+    node_to_index = {node: index for index, node in enumerate(nodes)}
+
+    matrix = np.zeros((num_nodes, num_nodes))
+
+    for edge in graph.edges(data=True):
+        source, target, data = edge
+        source_index = node_to_index[source]
+        target_index = node_to_index[target]
+        matrix[source_index][target_index] = data['weight']
+
+    return matrix
+
+
+def monte_carlo_widest_path(graph, start_node, end_node, iterations=3000, randomness_factor=0.2):
+    best_path = None
+    best_min_width = -np.inf
+
+    for _ in range(iterations):
+        path = [start_node]
+        current_node = start_node
+        min_width = np.inf
+
+        while current_node != end_node:
+            next_nodes = [n for n, d in graph[current_node].items() if 'weight' in d]
+            if len(next_nodes) == 0:
+                break
+
+            if random.random() < randomness_factor:
+                chosen_node = random.choice(next_nodes)
+            else:
+                widths = [graph[current_node][n]['weight'] for n in next_nodes]
+                max_width_index = np.argmax(widths)
+                chosen_node = next_nodes[max_width_index]
+
+            if chosen_node in path:
+                break
+
+            min_width = min(min_width, graph[current_node][chosen_node]['weight'])
+            current_node = chosen_node
+            path.append(current_node)
+
+        if current_node == end_node and min_width > best_min_width:
+            best_path = path
+            best_min_width = min_width
+
+    return best_path, best_min_width
+
+
+def on_click(event):
+    global start_node, end_node
+    if event.xdata is None or event.ydata is None:
+        return
+    for node, node_pos in pos.items():
+        distance = (
+            (event.xdata - node_pos[0]) ** 2 + (event.ydata - node_pos[1]) ** 2) ** 0.5
+        if distance < 0.1:
+            if start_node is None:
+                start_node = node
+                draw_graph(G, highlight=[start_node])
+                canvas.draw()
+                messagebox.showinfo("Info", f"Start node set to {node}")
+            elif end_node is None and node != start_node:
+                end_node = node
+                draw_graph(G, highlight=[start_node, end_node])
+                canvas.draw()
+                messagebox.showinfo("Info", f"End node set to {node}")
+                widest_path, widest_min_width = monte_carlo_widest_path(G, start_node, end_node)
+                if widest_path:
+                    draw_path(G, widest_path, ax, pos)
+                    canvas.draw()
+                    print("Widest Path:", widest_path, "with widest minimum width:", widest_min_width)
+                    start_node, end_node = None, None
+            return
+    messagebox.showinfo("Info", "No node was clicked.")
+
+
+def modify_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    corrected_lines = []
+    for line in lines:
+        if 'GranularActivity_' in line:
+            parts = line.split('GranularActivity_')
+            identifier = 'GranularActivity_' + parts[1].split(' ')[0]
+            new_line = parts[0] + identifier + '\n'
+            corrected_lines.append(new_line)
+        else:
+            corrected_lines.append(line)
+    with open(file_path, 'w') as file:
+        file.writelines(corrected_lines)
+
+
+if __name__ == "__main__":
+    file_path = 'data/task_graph/multiple_demos/task_graph.txt'
+    # modify_file(file_path)
+    delay = 0.5
+    threshold = 30
+    node_color = '#4f5d75'
+    highlight_color = '#ffc857'
+    edge_color = '#bfc0c0'
+    start_color = '#db3a34'
+    end_color = '#177e89'
+    path = 'data/task_graph/multiple_demos'
+    root = tk.Tk()
+    root.wm_title("Visualization")
+    fig, ax = plt.subplots(figsize=(10, 7), dpi=100)
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    processor = ActivityProcessor(path, threshold)
+    rich_info = processor.rich_info
+    G = parse_graph(file_path, rich_info)
+    # pos = nx.circular_layout(G)
+    pos = nx.spring_layout(G)
+
+    start_node = None
+    end_node = None
+
+    canvas_widget = canvas.get_tk_widget()
+    canvas_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+    canvas.mpl_connect("button_press_event", on_click)
+
+    root.after(1000, lambda: build_graph(G, delay))
+    tk.mainloop()
